49508769414a241f9f4c0753ff01f317668776f0
Arjav Patel <arjav1528@gmail.com>
2024-11-01 23:03:15 +0530

    COMMIT#15

--- DSA Course/Chapter-3 Sorting Problems/sorthard.cpp
+++ DSA Course/Chapter-3 Sorting Problems/sorthard.cpp
@@ -1,32 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
-void fourSum(){
-    vector<int> arr = {-2,-1,0,0,1,2};
-    int target  = 0;
-    map<vector<int>,int> mpp;
-    for(int i=0;i<n;i++){
-        for(int j=(i+1);j<n;j++){
-            for(int k=(j+1);k<n;k++){
-                mpp[{i,j,k}] = arr[i]+arr[j]+arr[k];
+// void fourSum(){
+//     vector<int> arr = {-2,-1,0,0,1,2};
+//     int target  = 0;
+//     map<vector<int>,int> mpp;
+//     for(int i=0;i<n;i++){
+//         for(int j=(i+1);j<n;j++){
+//             for(int k=(j+1);k<n;k++){
+//                 mpp[{i,j,k}] = arr[i]+arr[j]+arr[k];
 
-            }
-        }
-    }
-    for(int i=0;i<n;i++){
-        for(auto it : mpp){
-            if((it.first[0] != i) && (it.first[1] != i) && (it.first[2] != i) && ((arr[i]+it.second) == target)){
-                set<int> temp ={}
-            }
-        }
-    }
+//             }
+//         }
+//     }
+//     for(int i=0;i<n;i++){
+//         for(auto it : mpp){
+//             if((it.first[0] != i) && (it.first[1] != i) && (it.first[2] != i) && ((arr[i]+it.second) == target)){
+//                 set<int> temp ={}
+//             }
+//         }
+//     }
 
-}  
+// }  
     
     
     
 
-}
+// }
 int main(){
-    threeSum();
+    // threeSum();
+    vector<vector<int>> temp = {{1,4},{0,0}};
+    sort(temp.begin(),temp.end());
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            cout<<temp[i][j]<<" ";
+        }
+        cout<<endl;
+    }
 
 }
