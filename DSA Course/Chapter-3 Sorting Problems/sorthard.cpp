0fddaba16dfbff33a35505b4bb808b87f98e241f
Arjav Patel <arjav1528@gmail.com>
2024-11-01 22:24:04 +0530

    COMMIT#14

--- DSA Course/Chapter-3 Sorting Problems/sorthard.cpp
+++ DSA Course/Chapter-3 Sorting Problems/sorthard.cpp
@@ -1,20 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-void threeSum(){
-    vector<int> arr = {-1,0,1,2,-1,-4};
-    sort(arr.begin(),arr.end());
-    int n = arr.size();
-    vector<vector<int>> ans;
+void fourSum(){
+    vector<int> arr = {-2,-1,0,0,1,2};
+    int target  = 0;
     map<vector<int>,int> mpp;
     for(int i=0;i<n;i++){
         for(int j=(i+1);j<n;j++){
-            mpp[{i,j}] = (arr[i] + arr[j]);
+            for(int k=(j+1);k<n;k++){
+                mpp[{i,j,k}] = arr[i]+arr[j]+arr[k];
+
+            }
         }
     }
-    for(auto it : mpp){
-        cout<<arr[it.first[0]]<<"+"<<arr[it.first[1]]<<"="<<it.second<<endl;
+    for(int i=0;i<n;i++){
+        for(auto it : mpp){
+            if((it.first[0] != i) && (it.first[1] != i) && (it.first[2] != i) && ((arr[i]+it.second) == target)){
+                set<int> temp ={}
+            }
+        }
     }
-    
+
+}  
     
     
     
