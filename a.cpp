d1d832e9c9dbecf320c1619781045bad96b073ff
Arjav Patel <arjav1528@gmail.com>
2024-11-12 23:55:31 +0530

    COMMIT#33

--- a.cpp
+++ a.cpp
@@ -3,21 +3,59 @@
 using namespace std;
 
 int main(){
-    string s = "tree";
-    unordered_map<char,int> m1;
-    for(int i=0;i<s.size();i++){
-        m1[s[i]]++;
-    }
-    map<int,string>mpp;
-    for(auto it : m1){
-        mpp[it.second] = it.first;
-    }
-    for(auto it : m1){
-        cout<<it.first<<" "<<it.second<<endl;
-    }
-    // string final = "";
-    // for(auto it: mpp){
-    //     final = it.second + final;
-    // }
-    // cout<<final;
+    string s = "-13+8";
+    int j=0;
+        while(j<s.size()){
+            if(s[j]==' '){
+                s.erase(s.begin());
+            }
+            else{
+                break;
+            }
+        }
+        long long int final = 0;
+        int count = 0;
+        if(s[0]=='-'){
+            if(s[1] == '+' || s[1]=='-'){
+                return 0;
+            }
+            else{
+                count++;
+                s.erase(s.begin());
+            }
+        }  
+        for(int i=0;i<s.size();i++){
+            // if(s[i]=='+'){
+            //     continue;
+            // }
+            
+            if((48 <= s[i]) && (s[i] <= 57)){
+                final = (final*10) + (s[i]-48);
+                if(count){
+                    final = final*-1;
+                    if(final<INT_MIN){
+                        cout<<INT_MIN;
+                        return 0;
+
+                    }
+                    final = final*-1;
+                }
+                else{
+                    if(final>INT_MAX){
+                        cout<<INT_MAX;
+                        return 0;
+                    }
+                }
+
+            }
+            else{
+                break;
+            }
+        }
+        if(count){
+            cout<<(final*(-1));
+        }
+        else{
+            cout<<final;
+        }
 }
