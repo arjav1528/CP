05694371a9e0a6b8c9ff037488ccebdde4935f18
Arjav Patel <arjav1528@gmail.com>
2024-11-12 11:48:47 +0530

    COMMIT#30

--- a.cpp
+++ a.cpp
@@ -1,11 +1,23 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
+
 int main(){
-    string s = "abcde";
-    string goal = "cdeab";
-    string str = s+s;
-    cout<<str.find(goal);
-    // cout<<s;
-    // return num;
+    string s = "tree";
+    unordered_map<char,int> m1;
+    for(int i=0;i<s.size();i++){
+        m1[s[i]]++;
+    }
+    map<int,string>mpp;
+    for(auto it : m1){
+        mpp[it.second] = it.first;
+    }
+    for(auto it : m1){
+        cout<<it.first<<" "<<it.second<<endl;
+    }
+    // string final = "";
+    // for(auto it: mpp){
+    //     final = it.second + final;
+    // }
+    // cout<<final;
 }
