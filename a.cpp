f2ec6b65af95f24cacb9dc98eabb7c9e1e358d2e
Arjav Patel <arjav1528@gmail.com>
2024-11-12 10:08:46 +0530

    COMMIT#27

--- a.cpp
+++ a.cpp
@@ -2,20 +2,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string num = "52";
-    string s;
-    for(int i=num.size()-1;i>=0;i--){
-        char t = num[i];
-
-            // string s = "";
-        if(t == 49 || t == 51 || t == 53 || t == 55 || t == 57){
-            s = num.substr(0,i);
-            break;
-            // return s;
-
-        }
-            
-    }
-    cout<<s;
+    string s = "abcde";
+    string goal = "cdeab";
+    string str = s+s;
+    cout<<str.find(goal);
+    // cout<<s;
     // return num;
 }
