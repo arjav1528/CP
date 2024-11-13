ba2fce78c6f0aa421793a8a901605eb58c44fbdb
Arjav Patel <arjav1528@gmail.com>
2024-11-13 16:10:28 +0530

    COMMIT#34

---
+++ LeetCode/5.longest-palindromic-substring.cpp
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode id=5 lang=cpp
+ *
+ * [5] Longest Palindromic Substring
+ *
+ * https://leetcode.com/problems/longest-palindromic-substring/description/
+ *
+ * algorithms
+ * Medium (34.72%)
+ * Likes:    29877
+ * Dislikes: 1837
+ * Total Accepted:    3.4M
+ * Total Submissions: 9.9M
+ * Testcase Example:  '"babad"'
+ *
+ * Given a string s, return the longest palindromic substring in s.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "babad"
+ * Output: "bab"
+ * Explanation: "aba" is also a valid answer.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "cbbd"
+ * Output: "bb"
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= s.length <= 1000
+ * s consist of only digits and English letters.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.empty()) {
+            return "";
+        }
+
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int odd = expandAroundCenter(s, i, i);
+            int even = expandAroundCenter(s, i, i + 1);
+            int max_len = max(odd, even);
+
+            if (max_len > end - start) {
+                start = i - (max_len - 1) / 2;
+                end = i + max_len / 2;
+            }
+        }
+
+        return s.substr(start, end - start + 1);        
+    }
+
+private:
+    int expandAroundCenter(string s, int left, int right) {
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }    
+};
+// @lc code=end
+
--- a.cpp
+++ a.cpp
@@ -3,59 +3,8 @@
 using namespace std;
 
 int main(){
-    string s = "-13+8";
-    int j=0;
-        while(j<s.size()){
-            if(s[j]==' '){
-                s.erase(s.begin());
-            }
-            else{
-                break;
-            }
-        }
-        long long int final = 0;
-        int count = 0;
-        if(s[0]=='-'){
-            if(s[1] == '+' || s[1]=='-'){
-                return 0;
-            }
-            else{
-                count++;
-                s.erase(s.begin());
-            }
-        }  
-        for(int i=0;i<s.size();i++){
-            // if(s[i]=='+'){
-            //     continue;
-            // }
-            
-            if((48 <= s[i]) && (s[i] <= 57)){
-                final = (final*10) + (s[i]-48);
-                if(count){
-                    final = final*-1;
-                    if(final<INT_MIN){
-                        cout<<INT_MIN;
-                        return 0;
-
-                    }
-                    final = final*-1;
-                }
-                else{
-                    if(final>INT_MAX){
-                        cout<<INT_MAX;
-                        return 0;
-                    }
-                }
-
-            }
-            else{
-                break;
-            }
-        }
-        if(count){
-            cout<<(final*(-1));
-        }
-        else{
-            cout<<final;
-        }
+    unordered_map<int,int> mpp;
+    mpp[2] = 1;
+    mpp[2]--;
+    cout<<mpp.size();
 }
