5da4a0845340055d4bda806c429e93e58f102420
Arjav Patel <arjav1528@gmail.com>
2024-11-15 00:38:12 +0530

    COMMIT#37

--- a.cpp
+++ a.cpp
@@ -1,53 +1,27 @@
-#include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-bool cmp(pair<char, int>& a, 
-        pair<char, int>& b) 
-{ 
-    return a.second < b.second; 
-}
-void sortMap(unordered_map<char, int>& M) 
-{ 
- 
-    // Declare vector of pairs 
-    vector<pair<char, int> > A; 
- 
-    // Copy key-value pair from Map 
-    // to vector of pairs 
-    for (auto& it : M) { 
-        A.push_back(it); 
-    } 
- 
-    // Sort using comparator function 
-    sort(A.begin(), A.end(), cmp); 
- 
-    // Print the sorted value 
-    // for (auto& it : A) { 
- 
-    //     cout << it.first << ' '
-    //         << it.second << endl; 
-    // } 
-}
-
-int main(){
-    string s = "aabcb";
-    int left = 0;
-    int right = 0;
-    while((left<=right) && (right<s.size())){
-        unordered_map<char,int> mpp;
-        for(int i=left;i<=right;i++){
-            mpp[s[i]]++;
-        }
-        sortMap(mpp);
-        cout<<mpp.begin()->first<<" "<<mpp.end()->second;
-        if(left<right){
-            right--;
-        }
-        else{
-            left++;
-            right = s.size()-1;
-        }
+class Node{
+    public:
+    int data;
+    Node* next;
+    // public :
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
     }
-        
-        
+    // public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+int main(){
+    vector<int> arr = {1,2,3,4,5,6};
+    Node* head = new Node(arr[0],nullptr);
+    Node* mover = head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
 }
