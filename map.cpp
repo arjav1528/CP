bd6c02268ca086f8041816d5bd3a58a8c8b1c2dc
Arjav Patel <161294839+arjav1528@users.noreply.github.com>
2024-11-18 23:56:29 +0530

    COMMIT#40

--- map.cpp
+++ map.cpp
@@ -2,13 +2,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    vector<int> arr = {100,4,200,1,3,2};
-    map<int,int> mpp;
-    for(int i=0;i<arr.size();i++){
-        mpp[arr[i]]++;
-    }
-    for(auto it : mpp){
-        cout<<it.first<<"-"<<it.second<<endl;
-    }
+    // vector<int> arr = {100,4,200,1,3,2};
+    // map<int,int> mpp;
+    // for(int i=0;i<arr.size();i++){
+    //     mpp[arr[i]]++;
+    // }
+    // for(auto it : mpp){
+    //     cout<<it.first<<"-"<<it.second<<endl;
+    // }
     
 }
