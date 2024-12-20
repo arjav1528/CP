8b121ddf2488ab9e2dfad32f81be98ee02b8da6d
Arjav Patel <arjav1528@gmail.com>
2024-12-20 15:53:13 +0530

    COMMIT#50

---
+++ Codeforces/A_Kevin_and_Combination_Lock.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    for(int w=0;w<t;w++){
+        int n;
+        cin>>n;
+        if(n%33){
+            cout<<"NO";
+        }
+        else{
+            cout<<"YES";
+        }
+        cout<<endl;
+
+    }
+}
