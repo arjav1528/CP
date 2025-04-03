de568f1576aa08498290fd403407ca4be59939fe
arjav1528 <arjav1528@gmail.com>
2025-04-04 03:24:09 +0530

    COMMIT#68

--- LeetCode/48.rotate-image.cpp
+++ LeetCode/48.rotate-image.cpp
@@ -50,11 +50,10 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int row = matrix.size();
-        int column = row;
+        
         reverse(matrix.begin(),matrix.end());
-        for(int i=0;i<row;i++){
-            for(int j=0;j<column;j++){
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix.size();j++){
                 if(i>j){
                     swap(matrix[i][j],matrix[j][i]);
                 }
