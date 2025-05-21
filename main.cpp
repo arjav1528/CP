310681f980aecaa9688f38a673b4b8e52d2407ea
Arjav Patel <arjav1528@gmail.com>
2025-05-21 23:09:37 +0530

    COMMIT#83

--- main.cpp
+++ main.cpp
@@ -83,6 +83,31 @@ bool canJump(vector<int>& nums) {
         
 }
 
+int jump(vector<int>& nums) {
+        
+
+    int count = 0;
+
+        int l = 0;
+        int r = 0;
+        int jumps = 0;
+
+        while(r<nums.size()-1){
+            int farthest = 0;
+
+            for(int i=l;i<=r;i++){
+                farthest = max(farthest,i+nums[i]);
+            }
+
+            l = r+1;
+            r = farthest;
+            jumps++;
+        }        
+
+    return jumps;
+        
+}
+
 
 
 
@@ -92,7 +117,7 @@ bool canJump(vector<int>& nums) {
 int main(){
     vector<int> nums = {2,3,1,1,4};
 
-    cout<<canJump(nums);
+    cout<<jump(nums);
 
 
 
