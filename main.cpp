14de96f81c41d48e3fe75e85a119b19bf019c879
Arjav Patel <arjav1528@gmail.com>
2025-05-22 01:44:40 +0530

    COMMIT#84

--- main.cpp
+++ main.cpp
@@ -108,6 +108,40 @@ int jump(vector<int>& nums) {
         
 }
 
+bool customSort(pair<int,int> a, pair<int,int>b){
+    return a.first < b.first;
+}
+
+int findPlatform(vector<int>& Arrival, vector<int>& Departure){
+
+    vector<pair<int,char>> arr;
+
+    for(int i=0;i<Arrival.size();i++){
+        arr.emplace_back(Arrival[i],'A');
+        arr.emplace_back(Departure[i],'D');
+    }
+
+    sort(arr.begin(),arr.end(),customSort);
+   
+    int len = 0;
+    int maxLen = 0;
+
+    for(int i=0;i<arr.size();i++){
+        if(arr[i].second == 'A'){
+            len++;
+        }else{
+            maxLen = max(maxLen,len);
+            len = 0;
+        }
+    }
+
+    return maxLen;
+
+
+
+
+}
+
 
 
 
@@ -115,9 +149,13 @@ int jump(vector<int>& nums) {
 
 
 int main(){
-    vector<int> nums = {2,3,1,1,4};
+    
+    vector<int> arrival = {900, 940, 950, 1100, 1500, 1800};
+    vector<int> departure = {910, 1200, 1120, 1130, 1900, 2000};
 
-    cout<<jump(nums);
+    cout<<findPlatform(arrival,departure);
+
+
 
 
 
