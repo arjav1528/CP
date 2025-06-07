10e157d90409e5cc09cc5bdf14b8f03a672b5833
Arjav Patel <arjav1528>
2025-06-08 03:33:51 +0530

    COMMIT#125

--- main.cpp
+++ main.cpp
@@ -92,66 +92,25 @@ vector<int> dfsOfGraph(int V, vector<int> adj[]){
 
 }
 
-int recurse(int index, int target, vector<int>& nums, vector<vector<int>> &dp, int offset) {
+int recurse(int index, int ) {
      
-    if(dp[index][target + offset] != INT_MIN){
-        return dp[index][target + offset];
-    }
-    if(index == 0){
-        return (target == nums[0]) + (target == -nums[0]);
-    }
-
-    int l = recurse(index-1, target+nums[index], nums, dp, offset);
-    int r = recurse(index-1, target-nums[index], nums, dp, offset);
-
-    return dp[index][target + offset] = l+r;
+    if(index == 0)
 }
 
 
-int findTargetSumWays(vector<int>& nums, int target) {
-    int totalSum = 0;
-    for(int i = 0; i < nums.size(); i++) {
-        totalSum += nums[i];
-    }
-    
-    if(abs(target) > totalSum) return 0;
-    
-    if((totalSum - target) % 2 != 0) return 0;
-    
-    int sum = (totalSum + target) / 2;
-    if(sum < 0) return 0;
-    
-    int n = nums.size();
-    vector<int> prev(sum + 1, 0), curr(sum + 1, 0);
-    
-    prev[0] = 1;
-    
-    if(nums[0] <= sum) {
-        prev[nums[0]] = 1;
-    }
-    
-    for(int i = 1; i < n; i++) {
-        for(int j = 0; j <= sum; j++) {
-            int notTake = prev[j];
-            int take = 0;
-            if(nums[i] <= j) {
-                take = prev[j - nums[i]];
-            }
-            curr[j] = notTake + take;
-        }
-        prev = curr;
-    }
-    
-    return prev[sum];
+
+int paintWalls(vector<int>& cost, vector<int>& time) {
+        
 }
 
 int main(){
 
-    vector<int> q = {1,1,1,1,1};
+    vector<int> q = {1,2,3,2};
+    vector<int> q2 = {1,2,3,2};
     int target = 3;
     vector<vector<int>> que = {{2,1,3},{6,5,4},{7,8,9}};
 
-    cout<<findTargetSumWays(q,target);
+    cout<<paintWalls(q,q2);
     
 
 }
