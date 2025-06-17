088ce3692001598bfc8763e329d2d632e0858241
Arjav Patel <arjav1528@gmail.com>
2025-06-17 14:24:23 +0530

    COMMIT#133

--- main.cpp
+++ main.cpp
@@ -34,50 +34,87 @@ class Node{
 };
 
 
-void dfs(int node, vector<vector<int>>& adj, vector<int>& visited) {
-    visited[node] = 1;
-    for (int neighbor : adj[node]) {
-        if (!visited[neighbor]) {
-            dfs(neighbor, adj, visited);
-        }
-    }
-}
+int orangesRotting(vector<vector<int>>& grid) {
 
-int findCircleNum(vector<vector<int>>& isConnected) {
-    int n = isConnected.size();
-    vector<vector<int>> adj(n);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (isConnected[i][j] == 1 && i != j) {
-                adj[j].push_back(i);
+    queue<pair<pair<int,int>,int>> q;
+    int n = grid.size();
+    int m = grid[0].size();
+    int vis[n][m];
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(grid[i][j] == 2){
+                q.push({{i,j},0});
+                vis[i][j] = 2;
+            }else{
+                vis[i][j] = 0;
             }
         }
     }
 
-    // cout << "Adjacency List:" << endl;
-    // for (int i = 0; i < n; i++) {
-    //     cout << i << ": ";
-    //     for (int neighbor : adj[i]) {
-    //         cout << neighbor << " ";
-    //     }
-    //     cout << endl;
-    // }
-    int count = 0;
-    vector<int> visited(n, 0);
-    for (int i = 0; i < n; i++) {
-        if (!visited[i]) {
-            count++;
-            dfs(i, adj, visited);
+    int tm = 0;
+    vector<int> drow = {-1,0,1,0};
+    vector<int> dcol = {0,1,0,-1};
+
+    while(!q.empty()){
+        int r = q.front().first.first;
+        int c = q.front().first.second;
+        int t = q.front().second;
+        q.pop();
+
+        tm = max(t,tm);
+
+        for(int i=0;i<4;i++){
+            int nrow = r + drow[i];
+            int ncol = c + dcol[i];
+
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]!=2 && grid[nrow][ncol]==1){
+                q.push({{nrow,ncol},t+1});
+                vis[nrow][ncol] = 2;
+
+            }
         }
     }
-    return count;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(vis[i][j] != 2 && grid[i][j] ==1){
+                return -1;
+            }
+        }
+    }
+    return tm;
+        
 }
 int main(){
 
-    vector<vector<int>> isConnected = {{1,1,0},{1,1,0},{0,0,1}};
+    vector<vector<int>> grid;
+    string input;
+    cin >> input;
 
-    cout<<findCircleNum(isConnected);
-    
+    vector<int> row;
+    int num = 0;
+    bool building_num = false;
+    for (size_t i = 0; i < input.size(); ++i) {
+        char ch = input[i];
+        if (isdigit(ch)) {
+            num = num * 10 + (ch - '0');
+            building_num = true;
+        } else if (ch == ',' || ch == ']') {
+            if (building_num) {
+                row.push_back(num);
+                num = 0;
+                building_num = false;
+            }
+            if (ch == ']') {
+                if (!row.empty()) {
+                    grid.push_back(row);
+                    row.clear();
+                }
+            }
+        }
+    }
 
+    cout << orangesRotting(grid);
 }
 
